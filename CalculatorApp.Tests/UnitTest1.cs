cae9778966e4e4c8ee6900a183bc764eae856822
pofexwere <lolimariason@gmail.com>
2026-05-21 08:54:40 +0300

    Добавьте файлы проекта.

---
+++ CalculatorApp.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿namespace CalculatorApp.Tests_
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void Test1()
+		{
+			Calculator c = new Calculator();
+			int result = c.Add(5, 3);
+			Assert.Equal(8, result);
+		}
+	}
+}
---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CalculatorApp
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Calculator c = new Calculator();
+			Console.WriteLine(c.Add(1,3));
+		}
+	}
+}
